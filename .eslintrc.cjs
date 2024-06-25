3d6cf1ed43369f694abd34b9f390d8fa97c4791f
Sergey <sergsmile29@gmail.com>
2024-06-25 20:23:49 +0300

    change app.jsx

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,10 +12,8 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'react/prop-types': 'off',
     'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
