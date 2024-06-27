d0f3ef064b8bb6a4874383e0755e883109ac9a12
Sergey <sergsmile29@gmail.com>
2024-06-27 21:08:18 +0300

    react icons

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,7 @@
 import clsx from 'clsx';
 import s from './Profile.module.css';
+import { FaUser, FaPhoneAlt } from 'react-icons/fa';
+
 const Profile = ({ user }) => {
   return (
     <article className={s.wrapper}>
@@ -9,10 +11,12 @@ const Profile = ({ user }) => {
         </div>
         <div className={s.content}>
           <h2>
-            {user.lastName} {user.firstName}
+            <FaUser className={s.icon} /> {user.lastName} {user.firstName}
           </h2>
           <p>Email: {user.email}</p>
-          <p>Phone: {user.phone}</p>
+          <p>
+            Phone: <FaPhoneAlt /> {user.phone}
+          </p>
           <p className={clsx(user.age > 18 ? s.green : s.red)}>Age: {user.age}</p>
           <p>Gender: {user.gender}</p>
           <p>
