8dde234a077cb73342b44859e41554df7bc7c981
Sergey <sergsmile29@gmail.com>
2024-07-04 20:41:28 +0300

    logic modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import { ColorPicker } from './ColorPicker/ColorPicker';
 import { Counter } from './Counter/Counter';
+import Modal from './Modal/Modal';
 import { TodoList } from './TodoList/TodoList';
 export const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
   return (
     <>
-      <Counter />
+      <button onClick={handleOpenModal}>Open modal</button>
+      {isOpen && (
+        <Modal onClose={handleCloseModal}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam temporibus odit fugit laudantium
+          perferendis reprehenderit deleniti vitae sed ab! Sed nam at dolore a magni architecto quos vitae natus velit?
+        </Modal>
+      )}
+
       {/* <TodoList /> */}
       {/* <ColorPicker /> */}
     </>
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,47 @@
+import { useEffect } from 'react';
 import s from './Modal.module.css';
-const Modal = ({ children, title = 'Default modal' }) => {
+const Modal = ({ children, onClose, title = 'Default modal' }) => {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      console.log(e.key);
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    console.log('Модальне вікно відмалювалось!');
+    const intervalID = setInterval(() => {
+      console.log(new Date().toLocaleTimeString());
+    }, 1000);
+    const timeoutID = setTimeout(() => {
+      console.log('Badabum!!!🔥');
+    }, 3000);
+
+    return () => {
+      console.log('Модалка закрилась!');
+      document.removeEventListener('keydown', handleKeyDown);
+      clearInterval(intervalID);
+      clearTimeout(timeoutID);
+    };
+  }, [onClose]);
+
+  const handleBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
   return (
-    <div className={s.wrapper}>
+    <div className={s.wrapper} onClick={handleBackdropClick}>
       <div className={s.content}>
         <>
           <h1>{title}</h1>
           <hr />
         </>
-        <button className={s.closeBtn}>×</button>
+        <button onClick={onClose} className={s.closeBtn}>
+          close
+        </button>
         {children}
       </div>
     </div>
