e5e7ef26eae208f8332c15f6880820713439819d
Sergey <sergsmile29@gmail.com>
2024-07-04 20:08:58 +0300

    useEffect

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,22 +1,58 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import s from './Counter.module.css';
 
 export const Counter = () => {
   const [counter, setCounter] = useState(0);
+  const [step, setStep] = useState(1);
+
+  // Виконується лише один раз при етапі монтування!
+  useEffect(() => {
+    console.log('Вітаю, я відмалювався!');
+  }, []);
+
+  // Виконується ПЕРШИЙ раз і всі наступні, коли міняється лічильник!
+  useEffect(() => {
+    console.log('Counter is: ', counter);
+    if (counter === 10) {
+      setCounter(0);
+    }
+    if (counter < -5) {
+      console.log('Зачекай, не так далеко!');
+    }
+    if (counter === 5) {
+      console.log('Скоро в тебе буде 0');
+    }
+  }, [counter]);
+
+  useEffect(() => {
+    console.log('Step is:', step);
+  }, [step]);
+
+  useEffect(() => {
+    console.log('Змінились або лічильник, або крок');
+  }, [step, counter]);
+
+  //❌❌❌ Виконується кожен раз, коли оновлюється стейт або пропс
+  // useEffect(() => {
+  //   console.log(`Render ${step}`);
+  // });
 
   const handlePlusClick = () => {
-    setCounter(prevState => prevState + 1);
+    setCounter(prevState => prevState + step);
   };
   const handleMinusClick = () => {
-    setCounter(prev => prev - 1);
+    setCounter(prev => prev - step);
   };
   const handleReset = () => {
     setCounter(0);
+    setStep(1);
   };
+
   return (
     <div className={s.flexContainer}>
       <div className={s.wrapper}>
         <h1>{counter}</h1>
+        <input type='text' onChange={e => setStep(+e.target.value)} />
         <div className={s.flex}>
           <button onClick={() => handleMinusClick('Alex')} className='btn'>
             minus
