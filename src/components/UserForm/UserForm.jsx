e9d3d3469e1cdf807d82e17ca7bddc3637290579
Sergey <sergsmile29@gmail.com>
2024-07-11 20:07:58 +0300

    base form with formik

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,36 +1,58 @@
+import { Field, Form, Formik } from 'formik';
+
 export const UserForm = () => {
+  const initialValues = {
+    name: '',
+    surname: '',
+    age: '',
+    email: '',
+    role: 'user',
+    city: '',
+    about: '',
+  };
+  const handleSubmit = (data, actions) => {
+    console.log(data);
+    actions.resetForm();
+  };
+
   return (
     <div>
-      <form>
-        <label>
-          <span>Name:</span>
-          <input type='text' />
-        </label>
-        <label>
-          <span>Surname:</span>
-          <input type='surname' />
-        </label>
-        <label>
-          <span>Age:</span>
-          <input type='age' />
-        </label>
-        <label>
-          <span>Email:</span>
-          <input type='email' />
-        </label>
-        <label>
-          <span>Role</span>
-          <select>
-            <option value='user'>User</option>
-            <option value='admin'>Admin</option>
-          </select>
-        </label>
-        <label>
-          <span>About</span>
-          <textarea></textarea>
-        </label>
-        <button type='submit'>Create</button>
-      </form>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
+          <label>
+            <span>Name:</span>
+            <Field name='name' />
+          </label>
+          <label>
+            <span>Surname:</span>
+            <Field name='surname' />
+          </label>
+          <label>
+            <span>Age:</span>
+            <Field name='age' />
+          </label>
+          <label>
+            <span>Email:</span>
+            <Field name='email' />
+          </label>
+          <label>
+            <span>City:</span>
+            <Field name='city' />
+          </label>
+          <label>
+            <span>Role</span>
+            <Field name='role' as='select'>
+              <option value='user'>User</option>
+              <option value='admin'>Admin</option>
+            </Field>
+          </label>
+          <label>
+            <span>About</span>
+            <Field as='textarea' name='about'></Field>
+          </label>
+          <button type='submit'>Create</button>
+        </Form>
+      </Formik>
     </div>
   );
 };
