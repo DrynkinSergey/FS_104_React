5a062a94a72024e0fc624891317e00108f5562f6
Sergey <sergsmile29@gmail.com>
2024-07-15 23:53:58 +0300

    init m4l1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,4 @@
-import { UserForm } from './UserForm/UserForm';
-
 export const App = () => {
-  const register = data => {
-    console.log('Register in progress...');
-    setTimeout(() => {
-      console.log('Register done!');
-      console.log(data);
-    }, 3000);
-  };
-  return (
-    <>
-      <UserForm />
-    </>
-  );
+  return <div></div>;
 };
+//https://hn.algolia.com/api/v1/
