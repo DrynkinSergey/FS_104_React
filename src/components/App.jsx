abda310accf26686bc25dd6e805e2d65e2f8c9a3
Sergey <sergsmile29@gmail.com>
2024-07-16 21:20:16 +0300

    finally logic done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,13 +11,15 @@ export const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [page, setPage] = useState(0);
+  const [total, setTotal] = useState(0);
   useEffect(() => {
     const getData = async () => {
       try {
         setIsLoading(true);
         setIsError(false);
-        const response = await fetchNews(query, page);
+        const response = await fetchNews(query, page, 5);
         setHits(prev => [...prev, ...response.hits]);
+        setTotal(response.nbPages);
       } catch (error) {
         console.log(error);
         setIsError(true);
@@ -36,10 +38,10 @@ export const App = () => {
   return (
     <div>
       <SearchBar setQuery={handleSetQuery} />
-      {isLoading && <Loader />}
       {isError && <h2>Something went wrong! Try again...</h2>}
       <List items={hits} />
-      <button onClick={() => setPage(prev => prev + 1)}>Load more</button>
+      {isLoading && <Loader />}
+      {total > page && !isLoading && <button onClick={() => setPage(prev => prev + 1)}>Load more</button>}
     </div>
   );
 };
