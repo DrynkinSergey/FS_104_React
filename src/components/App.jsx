26d7f6ddcb1e4095dfa0007527b7847574d9d81f
Sergey <sergsmile29@gmail.com>
2024-07-16 19:50:54 +0300

    basic fetch with axios and then catch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from 'react';
+import List from './List/List';
+import axios from 'axios';
+import { useState } from 'react';
+
 export const App = () => {
-  return <div></div>;
+  const [hits, setHits] = useState([]);
+  useEffect(() => {
+    axios
+      .get('https://hn.algolia.com/api/v1/search?query=react')
+      .then(res => setHits(res.data.hits))
+      .catch();
+  }, []);
+  return (
+    <div>
+      <List items={hits} />
+    </div>
+  );
 };
 //https://hn.algolia.com/api/v1/
