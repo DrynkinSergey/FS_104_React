795079df3a5e162f3a8d9d7acd913a823b8bb346
Sergey <sergsmile29@gmail.com>
2024-07-30 19:58:39 +0300

    configure redux, create reducer, and connect to app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
 import 'modern-normalize';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <Provider store={store}>
     <App />
-  </>
+  </Provider>
 );
---
+++ src/redux/counter/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  counter: 0,
+  step: 1,
+};
+
+export const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
