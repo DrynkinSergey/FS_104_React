1cfea6d26e8142eb872a13032023c4ae3f175877
Sergey <sergsmile29@gmail.com>
2024-08-01 21:08:46 +0300

    redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { App } from './components/App';
 import 'modern-normalize';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
-
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,28 @@ import { configureStore } from '@reduxjs/toolkit';
 import { counterReducer } from './counter/slice';
 import { todosReducer } from './todos/slice';
 import { filterReducer } from './filter/slice';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, todosReducer);
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    todos: todosReducer,
+    todos: persistedReducer,
     filter: filterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
