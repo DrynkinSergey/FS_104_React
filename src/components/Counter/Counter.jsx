1fb722e50525d491f901b60cda1e5239d0b79853
SergiiOst <sergeyostapenko85@gmail.com>
2024-08-06 02:47:52 +0300

    Module 6 L1

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,28 +1,45 @@
-import { useSelector } from 'react-redux';
-import s from './Counter.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import s from "./Counter.module.css";
+import { selectCounter, selectStep } from "../../redux/counter/selectors";
+import {
+  changeStep,
+  decrement,
+  increment,
+  reset,
+} from "../../redux/counter/actions";
 
 export const Counter = () => {
   // йде до редакс і отримує данні від нього
-  const counter = useSelector(state => state.counter.counter);
-  const step = useSelector(state => state.counter.step);
+  const counter = useSelector(selectCounter);
+  const step = useSelector(selectStep);
 
-  const handlePlusClick = () => {};
-  const handleMinusClick = () => {};
-  const handleResetClick = () => {};
-  const handleChangeStep = e => {};
+  const dispatch = useDispatch();
+
+  const handlePlusClick = () => {
+    dispatch(increment());
+  };
+  const handleMinusClick = () => {
+    dispatch(decrement());
+  };
+  const handleResetClick = () => {
+    dispatch(reset());
+  };
+  const handleChangeStep = (e) => {
+    dispatch(changeStep(Number(e.target.value)));
+  };
   return (
     <div className={s.flexContainer}>
       <div className={s.wrapper}>
         <h1>{counter}</h1>
         <input value={step} onChange={handleChangeStep} />
         <div className={s.flex}>
-          <button className='btn' onClick={handleMinusClick}>
+          <button className="btn" onClick={handleMinusClick}>
             minus
           </button>
-          <button className='btn' onClick={handleResetClick}>
+          <button className="btn" onClick={handleResetClick}>
             reset
           </button>
-          <button className='btn' onClick={handlePlusClick}>
+          <button className="btn" onClick={handlePlusClick}>
             plus
           </button>
         </div>
--- src/redux/counter/reducer.js
+++ src/redux/counter/reducer.js
@@ -1,3 +1,5 @@
+import { CHANGE_STEP, DECREMENT, INCREMENT, RESET } from "./constants";
+
 const initialState = {
   counter: 0,
   step: 1,
@@ -5,6 +7,27 @@ const initialState = {
 
 export const counterReducer = (state = initialState, action) => {
   switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + state.step,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - state.step,
+      };
+    case RESET:
+      return {
+        ...state,
+        counter: 0,
+        step: 1,
+      };
+    case CHANGE_STEP:
+      return {
+        ...state,
+        step: action.payload,
+      };
     default:
       return state;
   }
